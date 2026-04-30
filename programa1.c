4b0eed86496cf282ca6ca421778fc5e249de6a3a
aurorasolertovar <aurora.fi1234@gmail.com>
2026-04-30 13:53:05 -0600

    Create programa1.c

--- programa1.c
+++ programa1.c
@@ -4,14 +4,19 @@
 void imprimir(char[]);
 // Definición o implementación de la función main
 int main(){
-    char nombre[] = "Facultad de Ingeniería"; 
+    char nombre[] = "Facultad de Ingenieria"; 
     imprimir(nombre);
 }
 // Implementación de las funciones del programa
 void imprimir(char s[]){ 
     int tam;
+    tam=strlen(s);
+    printf("El tamaño del string es %d\n", tam);
     for ( tam=strlen(s)-1 ; tam>=0 ; tam--) 
-        printf("%c", s[tam]);
+    {
+        printf("El tamaño del string es %d\n", tam);
+        printf("El %d es %c\n", tam, s[tam]);
     printf("\n"); 
+    }
     
 }
